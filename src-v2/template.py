0c6f60b6a276f3136c29b8f53dee6087ebf8866d
stefenmax <45379408+stefenmax@users.noreply.github.com>
2023-03-30 17:34:23 -0400

    🎉🎉 Add FBPConvNet & REDCNN

--- src-v2/template.py
+++ src-v2/template.py
@@ -58,9 +58,44 @@ def set_template(args):
         args.n_resblocks = 32
         args.n_feats = 256
         args.res_scale = 0.1
+        
     if args.template.find('RID') >= 0:
         args.data_train = 'AAPM'
         args.data_test = 'AAPM'
-        args.model = 'ridnet'
+        args.model = 'RIDNET'
         args.n_feats = 64
-
+        
+    if args.template.find('FBPCONV') >= 0:
+        args.data_train = 'AAPM'
+        args.data_test = 'AAPM'
+        args.model = 'FBPCONV'
+        args.epochs = 100
+        args.loss = '1*MSE'
+        args.lr = 1e-2
+        args.final_lr = 1e-3
+        args.lr_adjust = 'logarithmic'
+        args.batch_size = 1
+        args.optimizer = 'SGD'
+        args.momentum = 0.99
+        
+    if args.template.find('RED') >= 0:
+        args.data_train = 'AAPM'
+        args.data_test = 'AAPM'
+        args.model = 'REDCNN'
+        args.n_feats = 96
+        args.patch_size = 55
+        args.lr = 1e-4
+        args.final_lr = 1e-5
+        args.lr_adjust = 'logarithmic'
+        args.loss = '1*MSE'
+        
+    if args.template.find('DDNET') >= 0:
+        args.data_train = 'AAPM'
+        args.data_test = 'AAPM'
+        args.model = 'DDNET'
+        args.patch_size = 64
+        args.lr = 1e-4
+        args.final_lr = 1e-5
+        args.lr_adjust = 'logarithmic'
+        args.loss = '1*MSE'
+        args.batch_size = 5
